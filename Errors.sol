f880e93879b7fc6e59a05e45f79df980f975ec70
Jade Of Wallstreet <samuelchimmy@gmail.com>
2024-04-27 00:05:36 +0100

    Update Errors.sol

--- Errors.sol
+++ Errors.sol
@@ -1,133 +1,78 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
 
-// Importing OpenZeppelin ERC721 contract
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+pragma solidity ^0.8.17;
 
-// Interface for interacting with a submission contract
-interface ISubmission {
-    // Struct representing a haiku
-    struct Haiku {
-        address author; // Address of the haiku author
-        string line1; // First line of the haiku
-        string line2; // Second line of the haiku
-        string line3; // Third line of the haiku
+contract ErrorTriageExercise {
+    /**
+     * @dev Finds the difference between each uint with its neighbor (a to b, b to c, etc.)
+     * and returns a uint array with the absolute integer difference of each pairing.
+     * 
+     * @param _a The first unsigned integer.
+     * @param _b The second unsigned integer.
+     * @param _c The third unsigned integer.
+     * @param _d The fourth unsigned integer.
+     * 
+     * @return results An array containing the absolute differences between each pair of integers.
+     */
+    function diffWithNeighbor(
+        uint _a,
+        uint _b,
+        uint _c,
+        uint _d
+    ) public pure returns (uint[] memory) {
+        // Initialize an array to store the differences
+        uint[] memory results = new uint[](3);
+
+        // Calculate the absolute difference between each pair of integers and store it in the results array
+        results[0] = _a > _b ? _a - _b : _b - _a;
+        results[1] = _b > _c ? _b - _c : _c - _b;
+        results[2] = _c > _d ? _c - _d : _d - _c;
+
+        // Return the array of differences
+        return results;
     }
 
-    // Function to mint a new haiku
-    function mintHaiku(
-        string memory _line1,
-        string memory _line2,
-        string memory _line3
-    ) external;
-
-    // Function to get the total number of haikus
-    function counter() external view returns (uint256);
-
-    // Function to share a haiku with another address
-    function shareHaiku(uint256 _id, address _to) external;
-
-    // Function to get haikus shared with the caller
-    function getMySharedHaikus() external view returns (Haiku[] memory);
-}
-
-// Contract for managing Haiku NFTs
-contract HaikuNFT is ERC721, ISubmission {
-    Haiku[] public haikus; // Array to store haikus
-    mapping(address => mapping(uint256 => bool)) public sharedHaikus; // Mapping to track shared haikus
-    uint256 public haikuCounter; // Counter for total haikus minted
-
-    // Constructor to initialize the ERC721 contract
-    constructor() ERC721("HaikuNFT", "HAIKU") {
-        haikuCounter = 1; // Initialize haiku counter
-    }
-
-    string salt = "value"; // A private string variable
-
-    // Function to get the total number of haikus
-    function counter() external view override returns (uint256) {
-        return haikuCounter;
-    }
-
-    // Function to mint a new haiku
-    function mintHaiku(
-        string memory _line1,
-        string memory _line2,
-        string memory _line3
-    ) external override {
-        // Check if the haiku is unique
-        string[3] memory haikusStrings = [_line1, _line2, _line3];
-        for (uint256 li = 0; li < haikusStrings.length; li++) {
-            string memory newLine = haikusStrings[li];
-            for (uint256 i = 0; i < haikus.length; i++) {
-                Haiku memory existingHaiku = haikus[i];
-                string[3] memory existingHaikuStrings = [
-                    existingHaiku.line1,
-                    existingHaiku.line2,
-                    existingHaiku.line3
-                ];
-                for (uint256 eHsi = 0; eHsi < 3; eHsi++) {
-                    string memory existingHaikuString = existingHaikuStrings[
-                        eHsi
-                    ];
-                    if (
-                        keccak256(abi.encodePacked(existingHaikuString)) ==
-                        keccak256(abi.encodePacked(newLine))
-                    ) {
-                        revert HaikuNotUnique();
-                    }
-                }
-            }
+    /**
+     * @dev Changes the base by the value of the modifier. Base is always >= 1000. Modifiers can be
+     * between positive and negative 100.
+     * 
+     * @param _base The base value to be modified.
+     * @param _modifier The value by which the base should be modified.
+     * 
+     * @return returnValue The modified value of the base.
+     */
+    function applyModifier(
+        uint _base,
+        int _modifier
+    ) public pure returns (uint returnValue) {
+        // Apply the modifier to the base value
+        if(_modifier > 0) {
+            return _base + uint(_modifier);
         }
-
-        // Mint the haiku NFT
-        _safeMint(msg.sender, haikuCounter);
-        haikus.push(Haiku(msg.sender, _line1, _line2, _line3));
-        haikuCounter++;
+        return _base - uint(-_modifier);
     }
 
-    // Function to share a haiku with another address
-    function shareHaiku(uint256 _id, address _to) external override {
-        require(_id > 0 && _id <= haikuCounter, "Invalid haiku ID");
 
-        Haiku memory haikuToShare = haikus[_id - 1];
-        require(haikuToShare.author == msg.sender, "NotYourHaiku");
+    uint[] arr;
 
-        sharedHaikus[_to][_id] = true;
+    function popWithReturn() public returns (uint returnNum) {
+        if(arr.length > 0) {
+            uint result = arr[arr.length - 1];
+            arr.pop();
+            return result;
+        }
     }
 
-    // Function to get haikus shared with the caller
-    function getMySharedHaikus()
-        external
-        view
-        override
-        returns (Haiku[] memory)
-    {
-        uint256 sharedHaikuCount;
-        for (uint256 i = 0; i < haikus.length; i++) {
-            if (sharedHaikus[msg.sender][i + 1]) {
-                sharedHaikuCount++;
-            }
-        }
-
-        Haiku[] memory result = new Haiku[](sharedHaikuCount);
-        uint256 currentIndex;
-        for (uint256 i = 0; i < haikus.length; i++) {
-            if (sharedHaikus[msg.sender][i + 1]) {
-                result[currentIndex] = haikus[i];
-                currentIndex++;
-            }
-        }
-
-        if (sharedHaikuCount == 0) {
-            revert NoHaikusShared();
-        }
-
-        return result;
+    // The utility functions below are working as expected
+    function addToArr(uint _num) public {
+        arr.push(_num);
     }
 
-    // Custom errors
-    error HaikuNotUnique(); // Error for attempting to mint a non-unique haiku
-    error NotYourHaiku(); // Error for attempting to share a haiku not owned by the caller
-    error NoHaikusShared(); // Error for no haikus shared with the caller
+    function getArr() public view returns (uint[] memory) {
+        return arr;
+    }
+
+    function resetArr() public {
+        delete arr;
+    }
 }
